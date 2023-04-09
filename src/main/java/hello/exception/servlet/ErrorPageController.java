ff0e309ec51838b72efbb4909d47513da29282d9
sunwooKim <frod5@kakao.com>
2023-04-09 22:00:10 +0900

    스프링 mvc 2편 - 오류 흐름 설명 주석 추가

--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -12,6 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("/error-page")
 public class ErrorPageController {
 
+    //1. WAS(여기까지 전파) <- 필터 <- 서블릿 <- 인터셉터 <- 컨트롤러(예외발생)
+    //2. WAS `/error-page/500` 다시 요청 -> 필터 -> 서블릿 -> 인터셉터 -> 컨트롤러(/errorpage/500) -> View
+
     @RequestMapping("/404")
     public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
         log.info("errorPage 404");
