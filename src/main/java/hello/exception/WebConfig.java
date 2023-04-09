c0ce29577d2d9509beec9b59722d09a941eb860e
sunwooKim <frod5@kakao.com>
2023-04-09 22:24:42 +0900

    스프링 mvc 2편 - 예외 발생시 내부에서 정의해둔 에러페이지 URL을 다시 호출하여 필터를 2번 호출하게되는 흐름 실습.

---
+++ src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,24 @@
+package hello.exception;
+
+import hello.exception.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<Filter>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+        filterFilterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST,DispatcherType.ERROR);  //DispatchType이 고객요청, 내부 에러요청 모두 필터 호출된다. 아무것도 입력하지 않으면 DispatcherType.REQUEST이 default
+        return filterFilterRegistrationBean;
+    }
+}
